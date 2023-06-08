63f0fdafd7349aecb97d53e5ec282fc74306d655
Pakeg Bae <pakegpakeg@gmail.com>
2023-06-08 22:00:56 +0000

    Update 14 files

---
+++ app/context/AuthContext.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { SessionProvider } from "next-auth/react";
+
+interface IAuthContextProps {
+  children: React.ReactNode;
+}
+
+export default function AuthContext({ children }: IAuthContextProps) {
+  return <SessionProvider> {children} </SessionProvider>;
+}
