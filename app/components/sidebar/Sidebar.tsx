3bd4051cbe90894a6ea0b6e98e33faa9f0fa7b72
Pakeg Bae <pakegpakeg@gmail.com>
2023-06-10 22:56:03 +0000

    Update 9 files

--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -6,7 +6,7 @@ const Sidebar = async ({ children }: { children: React.ReactNode }) => {
   const currentUser = await getCurrentUser();
 
   return (
-    <div className="h-full">
+    <div className="h-screen">
       <DesktopSidebar currentUser={currentUser} />
       <MobileFooter />
       <main className="lg:pl-20 h-full">{children}</main>
