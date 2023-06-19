2a3ceac51f69874dde6bb3c9a2b5eff27cb76004
Pakeg Bae <pakegpakeg@gmail.com>
2023-06-19 11:16:37 +0000

    Update page.tsx

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image"
 import AuthForm from "./components/AuthForm"
 
 export default function Home() {
+  console.log(document.cookie)
   return <div className="flex flex-col min-h-screen justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
     <div className="sm:mx-auto sm:w-full sm:max-w-md">
       <Image alt="Logo" height="48" width="48" className="mx-auto w-auto" src="/logo.png"/>
